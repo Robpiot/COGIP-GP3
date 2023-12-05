d9a6493e5c1e049b4497681728b6a127f21a80c4
Robpiot <robin.piot@outlook.com>
2023-12-05 13:27:19 +0100

    fixing routes, and pages layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import "./assets/css/style.css";
+import Home from "./view/home.jsx";
+import Invoices from "./view/invoices.jsx";
+import Companies from "./view/companies.jsx";
+import Contacts from "./view/contacts.jsx";
 import Header from "./partials/header.jsx";
 import Footer from "./partials/footer.jsx";
 
 function App() {
   return (
     <div>
-      <Header />
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/contacts" element={<h1>Contacts</h1>} />
-        <Route path="/companies" element={<h1>Companies</h1>} />
-        <Route path="/invoices" element={<h1>Invoices</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/companies" element={<Companies />} />
+        <Route path="/invoices" element={<Invoices />} />
       </Routes>
-      <Footer />
     </div>
   );
 }

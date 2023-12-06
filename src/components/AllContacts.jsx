f43225e36d3ae465b2425f322b50c4d32cbd05d4
Rosa <rosalie.boulard@gmail.com>
2023-12-06 09:51:00 +0100

    adding all contacts & all companies + style

---
+++ src/components/AllContacts.jsx
@@ -0,0 +1,87 @@
+export function AllContacts() {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th className="name">Name</th>
+                    <th className="phone">Phone</th>
+                    <th className="mail">Mail</th>
+                    <th className="comp">Company</th>
+                    <th className="creation">Created at</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Peter Gregory  </td>
+                    <td>555-4567</td>
+                    <td>peter.gregory@raviga.com</td>
+                    <td>Raviga</td>
+                    <td>25/09/2020</td>
+                </tr>
+                <tr>
+                    <td>Cameron How</td>
+                    <td>555-8765</td>
+                    <td>cam.how@mutiny.net</td>
+                    <td>Mutiny</td>
+                    <td>25/09/2020</td>
+                </tr>
+                <tr>
+                    <td>Gavin Belson</td>
+                    <td>555-6354</td>
+                    <td>gavin@hooli.com</td>
+                    <td>Hooli</td>
+                    <td>25/09/2020</td>
+                </tr>
+                <tr>
+                    <td>Jian Yang</td>
+                    <td>555-8765</td>
+                    <td>jian.yan@phoque.off</td>
+                    <td>Phoque Off</td>
+                    <td>25/09/2020</td>
+                </tr>
+                <tr>
+                    <td>Bertram Gilfoyle</td>
+                    <td>555-5434</td>
+                    <td>gilfoy@piedpiper.com</td>
+                    <td>Pied Pipper</td>
+                    <td>25/09/2020</td>
+                </tr>
+                <tr>
+                    <td>Peter Gregory  </td>
+                    <td>555-4567</td>
+                    <td>peter.gregory@raviga.com</td>
+                    <td>Raviga</td>
+                    <td>25/09/2020</td>
+                </tr>
+                <tr>
+                    <td>Cameron How</td>
+                    <td>555-8765</td>
+                    <td>cam.how@mutiny.net</td>
+                    <td>Mutiny</td>
+                    <td>25/09/2020</td>
+                </tr>
+                <tr>
+                    <td>Gavin Belson</td>
+                    <td>555-6354</td>
+                    <td>gavin@hooli.com</td>
+                    <td>Hooli</td>
+                    <td>25/09/2020</td>
+                </tr>
+                <tr>
+                    <td>Jian Yang</td>
+                    <td>555-8765</td>
+                    <td>jian.yan@phoque.off</td>
+                    <td>Phoque Off</td>
+                    <td>25/09/2020</td>
+                </tr>
+                <tr>
+                    <td>Bertram Gilfoyle</td>
+                    <td>555-5434</td>
+                    <td>gilfoy@piedpiper.com</td>
+                    <td>Pied Pipper</td>
+                    <td>25/09/2020</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}

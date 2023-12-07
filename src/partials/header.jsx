d728a42a1cc0a42f55660ecc6408c63bcef2d1c9
Robpiot <robin.piot@outlook.com>
2023-12-07 11:28:33 +0100

    help

--- src/partials/header.jsx
+++ src/partials/header.jsx
@@ -1,17 +1,29 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faUser } from "@fortawesome/free-solid-svg-icons";
 import "../assets/css/style.css";
-import { Route, Routes, Link } from "react-router-dom";
-import HeaderNavBarLinks from "../components/headerNavBar";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="headerDiv">
-      <button className="menuButton" onClick={toggleMenu}>
+      <button className="menuButton">
         <FontAwesomeIcon icon={faBars} style={{ color: "#000000" }} />
       </button>
-      <HeaderNavBarLinks />
+      <ul className="ulheaderLinks">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/invoices">Invoices</Link>
+        </li>
+        <li>
+          <Link to="/companies">Companies</Link>
+        </li>
+        <li>
+          <Link to="/contacts">Contacts</Link>
+        </li>
+      </ul>
 
       <nav className="navBarHeader">
         <ul className="ulTitleHeader">
@@ -21,7 +33,9 @@ const Header = () => {
             </Link>
           </li>
         </ul>
-
+        <button className="userButton">
+          <FontAwesomeIcon icon={faUser} style={{ color: "#4e511f" }} />
+        </button>
         <ul className="headerButtons">
           <li className="signupButton">Sign up</li>
           <li className="loginButton">Login</li>

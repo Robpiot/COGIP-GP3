7bfa4e5508ecc5a1ccdf0fa4af4d476af52fa466
Robpiot <robin.piot@outlook.com>
2023-12-07 12:25:24 +0100

    navbar toggle is working

--- src/partials/header.jsx
+++ src/partials/header.jsx
@@ -5,27 +5,37 @@ import "../assets/css/style.css";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenuLinks = () => {
+    setIsOpen((open) => !isOpen);
+  };
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((open) => !isMenuOpen);
+  };
   return (
     <div className="headerDiv">
-      <button className="menuButton">
+      <button className="menuButton" onClick={toggleMenuLinks}>
         <FontAwesomeIcon icon={faBars} style={{ color: "#000000" }} />
       </button>
-      <ul className="ulheaderLinks">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/invoices">Invoices</Link>
-        </li>
-        <li>
-          <Link to="/companies">Companies</Link>
-        </li>
-        <li>
-          <Link to="/contacts">Contacts</Link>
-        </li>
-      </ul>
-
       <nav className="navBarHeader">
+        <ul className={`ulHeaderLinks ${isOpen ? "is-open" : ""}`}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/invoices">Invoices</Link>
+          </li>
+          <li>
+            <Link to="/companies">Companies</Link>
+          </li>
+          <li>
+            <Link to="/contacts">Contacts</Link>
+          </li>
+        </ul>
         <ul className="ulTitleHeader">
           <li className="titleHeader">
             <Link to="/">
@@ -33,14 +43,15 @@ const Header = () => {
             </Link>
           </li>
         </ul>
-        <button className="userButton">
-          <FontAwesomeIcon icon={faUser} style={{ color: "#4e511f" }} />
-        </button>
-        <ul className="headerButtons">
+
+        <ul className={`headerButtons ${isMenuOpen ? "is-open" : ""}`}>
           <li className="signupButton">Sign up</li>
           <li className="loginButton">Login</li>
         </ul>
       </nav>
+      <button className="userButton" onClick={toggleMenu}>
+        <FontAwesomeIcon icon={faUser} style={{ color: "#4e511f" }} />
+      </button>
     </div>
   );
 };

9ee86a47c8993adf1eaa1927196f57034d977511
Carole Gérard <gerard.carole78@gmail.com>
2023-12-19 16:09:36 +0100

    update DashboardInfos.jsx (svg -> img src={} )

--- src/components/dashboard/DashboardInfos.jsx
+++ src/components/dashboard/DashboardInfos.jsx
@@ -6,6 +6,10 @@ import formatDate from "../../functions/dateFormat";
 
 import { UserContext } from "../../assets/utils/UserContext";
 
+import DeleteIcon from '../../assets/icons/delete.svg';
+import EditIcon from '../../assets/icons/edit.svg';
+import CancelIcon from '../../assets/icons/cancel.svg';
+
 const DashboardInfos = ({ data }) => {
 
     // console.log('data : ', data);
@@ -201,9 +205,7 @@ const DashboardInfos = ({ data }) => {
                                                 {((user.role_id === 1) || (user.role_id === 2 && data.dataName !== 'contacts')) && (
                                                     <>
                                                         <button className="btn" onClick={async () => await deleteRow(data.dataName, row.id)}>
-                                                            <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512">
-                                                                <path fill="#000000" d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/>
-                                                            </svg>
+                                                            <img src={DeleteIcon} alt="Supprimer" />
                                                         </button>
                                                     </>
                                                 )}
@@ -215,14 +217,10 @@ const DashboardInfos = ({ data }) => {
                                                 <td>
                                                     <input type="text" placeholder={row.col_1} ref={inputRefCol1} />
                                                     <button className="btn" onClick={() => handleEditRow(row, 'col_1')}>
-                                                        <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
-                                                            <path fill="#000000" d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/>
-                                                        </svg>
+                                                        <img src={EditIcon} alt="Modifier" />
                                                     </button>
                                                     <button className="btn" onClick={() => setIsBeingEdited({...isBeingEdited, idRow: null, col_1: false})}>
-                                                        <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512">
-                                                            <path fill="#000000" d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/>
-                                                        </svg>
+                                                        <img src={CancelIcon} alt="Annuler" />
                                                     </button>
                                                 </td>
                                             ) : (
@@ -234,14 +232,10 @@ const DashboardInfos = ({ data }) => {
                                                 <td>
                                                     <input type="text" placeholder={row.col_2} ref={inputRefCol2} />
                                                     <button className="btn" onClick={() => handleEditRow(row, 'col_2')}>
-                                                        <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
-                                                            <path fill="#000000" d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/>
-                                                        </svg>
+                                                        <img src={EditIcon} alt="Modifier" />
                                                     </button>
                                                     <button className="btn" onClick={() => setIsBeingEdited({...isBeingEdited, idRow: null, col_2: false})}>
-                                                        <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512">
-                                                            <path fill="#000000" d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/>
-                                                        </svg>
+                                                        <img src={CancelIcon} alt="Annuler" />
                                                     </button>
                                                 </td>
                                             ) : (
@@ -262,14 +256,10 @@ const DashboardInfos = ({ data }) => {
                                                         <input type="text" placeholder={row.col_3} ref={inputRefCol3} />
                                                     )}
                                                     <button className="btn" onClick={() => handleEditRow(row, 'col_3')}>
-                                                        <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
-                                                            <path fill="#000000" d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/>
-                                                        </svg>
+                                                        <img src={EditIcon} alt="Modifier" />
                                                     </button>
                                                     <button className="btn" onClick={() => setIsBeingEdited({...isBeingEdited, idRow: null, col_3: false})}>
-                                                        <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512">
-                                                            <path fill="#000000" d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/>
-                                                        </svg>
+                                                        <img src={CancelIcon} alt="Annuler" />
                                                     </button>
                                                 </td>
                                             ) : (
@@ -290,14 +280,10 @@ const DashboardInfos = ({ data }) => {
                                                         <input type="text" placeholder={row.col_4} ref={inputRefCol4} />
                                                     )}
                                                     <button className="btn" onClick={() => handleEditRow(row, 'col_4')}>
-                                                        <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
-                                                            <path fill="#000000" d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/>
-                                                        </svg>
+                                                        <img src={EditIcon} alt="Modifier" />
                                                     </button>
                                                     <button className="btn" onClick={() => setIsBeingEdited({...isBeingEdited, idRow: null, col_4: false})}>
-                                                        <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512">
-                                                            <path fill="#000000" d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/>
-                                                        </svg>
+                                                        <img src={CancelIcon} alt="Annuler" />
                                                     </button>
                                                 </td>
                                             ) : (
@@ -313,9 +299,7 @@ const DashboardInfos = ({ data }) => {
                                                 {((user.role_id === 1) || (user.role_id === 2 && data.dataName !== 'contacts')) && (
                                                     <>
                                                         <button className="btn" onClick={async () => await deleteRow(data.dataName, row.id)}>
-                                                            <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512">
-                                                                <path fill="#000000" d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/>
-                                                            </svg>
+                                                            <img src={DeleteIcon} alt="Supprimer" />
                                                         </button>
                                                     </>
                                                 )}
